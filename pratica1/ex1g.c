1dd3af98ebb92847737dd674a5a72f6f5a4062ba
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2023-10-09 11:11:34 -0300

    ex1g completo

---
+++ pratica1/ex1g.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+double calculaFuncao(double a, double b){
+    double resposta;
+    resposta = log2(a+b);
+    return resposta;
+}
+
+int main(){
+    double x, y, resp;
+    scanf("%lf %lf", &x, &y);
+    resp = calculaFuncao(x,y);
+    printf("O log eh: %.2lf\n", resp);
+    return 0;
+}
