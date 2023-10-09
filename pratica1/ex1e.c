ec5187bbfbda31c449474b108fd1448ae523efe5
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2023-10-09 10:42:31 -0300

    ex1e.c completo

--- pratica1/ex1e.c
+++ pratica1/ex1e.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-float calculaTotal(){
-
+void calculaFuncao(double a, double b, double c){
+    double total;
+    total = sqrt(c+sqrt(a+sqrt(4+sqrt(b))));
+    printf("S eh: %lf\n",total);
 }
 
-int main(){
-    
-    return 0;
+int main() {
+// Declara uma variável para armazenar o valor de e
+double e = exp(3), pi = M_PI, x;
+scanf("%lf", &x);
+calculaFuncao(e, x, pi);
+return 0;
 }
