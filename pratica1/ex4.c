f3d0196065c0392ca73c03cb1a01c4523a49c34e
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2023-10-10 09:20:40 -0300

    ex 4 alterado

--- pratica1/ex4.c
+++ pratica1/ex4.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+int transformaVar(char a){
+    int x = (int)a - 48;
+    return x;
+}
+
 int main(){
     char a;
+    int var;
     printf("Digite um numero entre 0-9\n");
-    scanf("%c", &a);
-    int x = (int)a -48;
-    printf("%d",x);
+    scanf(" %c", &a);
+    var = transformaVar(a);
+    printf("%d\n", var);
     return 0;
 }
