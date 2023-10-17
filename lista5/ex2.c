bbb84c7beda352d383c76564a6715827368ca20b
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2023-10-17 08:45:07 -0300

    ex2 completo

---
+++ lista5/ex2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    scanf("%d", &x);
+    if (x%2==0)
+    {
+        printf("Eh par\n");
+    }else
+    {
+        printf("Eh impar\n");
+    }
+    
+    
+
+    return 0;
+}
