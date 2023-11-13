02564f1f142a5d7e6762135368e695c6a33d8b68
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2023-11-13 14:07:16 -0300

    ex2 lista 6

--- lista6/ex2.c
+++ lista6/ex2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(){
+void conta20minusculo(){
     char i='a';
     int count=1;
     while(i<'a'+20){
@@ -8,5 +8,29 @@ int main(){
         i++;
         count++;
     }
+}
+
+void conta20maiusculo(){
+    char i='A';
+    int count=1;
+    while(i<'A'+20){
+        printf("%d - %c\n",count,i);
+        i++;
+        count++;
+    }
+}
+
+void conta20invertido(){
+    int i=1;
+    while(i<=26){
+        printf("%d - %c\n",i,'z'-i+1);
+        i++;
+    }
+}
+
+int main(){
+    conta20minusculo();
+    conta20maiusculo();
+    conta20invertido();
     return 0;
 }
