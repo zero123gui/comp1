ebce5ae88e97dafc5348bd0379031bf97e66a3ea
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2023-11-21 09:37:38 -0300

    ex8 pratica3 completo

--- pratica3/ex8.c
+++ pratica3/ex8.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+void funcao1(){
+    int i, j;
+    for (i = 1; i <= 9; i++){
+        for (j=i+6; j >= i+4; j--){
+            if (i%2){
+                printf("i=%d j=%d\n",i,j);
+                j--;
+                printf("i=%d j=%d\n",i,j);
+                j--;
+                printf("i=%d j=%d\n",i,j);
+            }
+        }
+               
+    }   
+}
+
 int main(){
-    
+    funcao1();
     return 0;
 }
