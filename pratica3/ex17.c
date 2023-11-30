d5df868a8575ebeaf6cb4da35b203f797360f687
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2023-11-30 11:20:38 -0300

    ex17 pratica 3 completo

--- pratica3/ex17.c
+++ pratica3/ex17.c
@@ -20,7 +20,7 @@ int defineComputador(){
 
 int defineUsuario(){
     int usuario;
-    printf("0-Pedra\n1-Papel\n2-Tesoura\n3-Espada");
+    printf("0-Pedra\n1-Papel\n2-Tesoura\n3-Espada\n");
     scanf("%d", &usuario);
     return usuario;
 }
@@ -34,40 +34,54 @@ int defineResultado(){
     if(usuario == 3 && computador == 3){//os dois escolheram espada
         int espadaUser = defineEspada();
         int espadaComp = defineEspada();
-        printf("Espada do usuario: %d e Espada do computador\n",espadaUser,espadaComp);
-        if(espadaUser == 1 && espadaComp == 1 ||
-           espadaUser == 0 && espadaComp == 0){
+        printf("Espada do usuario: %d e Espada do computador: %d\n",espadaUser,espadaComp);
+        if((espadaUser == 1 && espadaComp == 1) || (espadaUser == 0 && espadaComp == 0)){
             resultado = 0;//empate retorna 0
         }else if(espadaUser == 1){
-            resultado = 1;//empate retorna 1
-        }else resultado = 2;//empate retorna 2    
+            resultado = 1;//usuario ganhou retorna 1
+        }else resultado = 2;//computador ganhou retorna 2    
     }else if(usuario == 3){//o usuario escolheu espada 
         int espadaUser = defineEspada();
         printf("Espada do usuario: %d",espadaUser);
         if(espadaUser==1){
-            resultado = 1;
-        }else resultado = 2;
+            resultado = 1;//user ganhou retorna 1
+        }else resultado = 2;//comp ganhou 
     }else if(computador == 3){
         int espadaComp = defineEspada();
         printf("Espada do Computador: %d", espadaComp);
         if(espadaComp == 1){
-            resultado = 2;
-        }else return 1;
+            resultado = 2;//comp ganhou
+        }else return 1;//user ganhou
     }else if (usuario == computador){
-        resultado = 0;
-    }
-    
-    
-    
-    
+        resultado = 0;//empate
+    }else if(
+            //pedra e tesoura
+            (usuario == 0 && computador == 2) ||
+            //papel e pedra
+            (usuario == 1 && computador == 0) ||
+            //tesoura e papel
+            (usuario == 2 && computador == 1)){
+        resultado = 1;//usuario ganhou
+    }else resultado = 2;
+    return resultado;
 }
 
 int main(){
     srand(time(NULL));
-    int resultado;
+    int resultado, scoreUser=0, scorePC=0;
     for (int i = 0; i < 5; i++){
         resultado = defineResultado();
+        if(resultado == 1){
+            scoreUser+=1;
+            printf("Usuario ganhou\n");
+        }else if(resultado == 2){
+            scorePC+=1;
+            printf("Computador ganhou\n");
+        }
     }
+    if(scoreUser==3){
+        printf("O jogador ganhou\nPlacar = %dx%d\n", scoreUser, scorePC);
+    }else printf("O computador ganhou\nPlacar = %dx%d\n", scoreUser, scorePC);
     
 
     return 0;
