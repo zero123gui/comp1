b3d26d0453ba27f0c1cde93873f67b85e2caa7d0
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2024-02-20 20:31:12 -0300

    ex9 pratica 4

---
+++ pratica4/ex9.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void imprimePar(int par[], int j){
+    for (int i = 0; i < j; i++)
+    {
+        printf("par[%d] = %d\n", i, par[i]);
+    }
+    
+}
+
+void imprimeImpar(int impar[], int k){
+    for (int i = 0; i <k; i++)
+    {
+        printf("impar[%d] = %d\n", i, impar[i]);
+    }
+    
+}
+
+int main(){
+    int x, j=0, k=0, impar[5], par[5];
+    for (int i = 0; i < 15; i++)
+    {
+        scanf("%d", &x);
+        if (x%2==0)
+        {
+            par[j] = x;
+            j++;
+        }else {
+            impar[k] = x;
+            k++;
+        }
+        if (j==5 || i==14)
+        {
+            imprimePar(par, j);
+            j=0;
+        }
+        if (k==5 || i==14)
+        {
+            imprimeImpar(impar, k);
+            k=0;
+        }
+        
+        
+    }
+    
+    return 0;
+}
