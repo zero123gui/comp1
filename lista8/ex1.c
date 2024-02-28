04dfcfe4ff4bcda9f5f7504bb896ae7e647afdf8
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2024-02-28 11:27:06 -0300

    ex1 lista8

--- lista8/ex1.c
+++ lista8/ex1.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
 int main(){
-    int mat[3][4];
-    
-    for (int i = 0; i < 3; i++)
+    int l=3,c=4,x,y;
+    char mat[l][c];
+    do
     {
-        for (int j = 0; j < 4; j++)
+        printf("Digite qual posicao do tabuleiro deseja marcar\n");
+        scanf("%d %d", &x, &y);    
+    } while ((x<=0 || x>3) || (y<=0 || y>4));
+    
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < c; j++)
         {
-            printf("- ");
+            if (i==(x-1) && j==(y-1))
+            {
+                mat[i][j] = 'X';
+            }else{
+                mat[i][j]='-';
+            }
+        }
+    }
+
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%c ",mat[i][j]);
         }
         printf("\n");
     }
