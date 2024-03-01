9bfaabbea5b4a2e71ebac0e262dacfee7382ebc2
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2024-03-01 08:33:34 -0300

    ex7 corrigido

--- lista9/ex7.c
+++ lista9/ex7.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+
 #define MAX 10
 
 int main(){
-    int i=0;
+    int resultado;
     char n1[MAX],n2[MAX];
     scanf("%s %s", n1,n2);
-    if (n1[i]<n2[i])
+    resultado = strcmp(n1,n2);
+    if (resultado == -1)
     {
         printf("%s\n%s",n1,n2);
-    }else{
+    }else if(resultado == 1){
         printf("%s\n%s",n2,n1);
-    }
+    }else printf("São iguais\n");
     
     return 0;
 }
