24e2f0a30a77b31d90e2ab3996faa5ca9246cfa7
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2024-03-20 10:07:47 -0300

    ex5 lista10

--- lista10/ex5.c
+++ lista10/ex5.c
@@ -10,7 +10,7 @@ Filme cadastroFilme(Filme filme){
     printf("Digite o codigo do filme: ");
     scanf("%d%*c", &filme.codigo);
     printf("Digite o nome do filme: ");
-    scanf("%[^\n]%*c", filme.nome);
+    scanf("%*c%[^\n]%*c", filme.nome);
     printf("Digite o numero da sala: ");
     scanf("%d%*c", &filme.salas);
     return filme;
@@ -20,19 +20,34 @@ void imprimeFilme(Filme filme){
     printf("Filme em cartaz: %s\n", filme.nome);
     printf("Codigo do filme: %d\n", filme.codigo);
     printf("Numero da sala: %d\n", filme.salas);
+    printf("\n\n");
 }
 
 int main(){
     Filme filme[5];
-    int qntd;
-    printf("Digite a quantidade de filmes: ");
-    scanf("%d%*c", &qntd);
-    for (int i = 0; i < qntd; i++)
-    {
+    int i=0;
+    char op;
+
+    do{
+       printf("(C)adastrar filme\n");
+       printf("(M)ostrar filmes cadastrados\n");
+       printf("(S)air\n");
+       scanf("%c%*c", &op);
+
+       if (op == 'c' || op == 'C')
+       {
         filme[i] = cadastroFilme(filme[i]);
-        imprimeFilme(filme[i]);
-    }
-    
+        i++;
+       }else if (op == 'm' || op == 'M')
+       {
+        for (int j = 0; j < i; j++)
+        {
+        imprimeFilme(filme[j]);
+        }
+       }
+       
+        
+    }while (op!='S'||op!='s');
 
     return 0;
 }
