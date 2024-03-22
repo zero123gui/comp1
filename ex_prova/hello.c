d8de793e87990a4fe2dcc61ec35ee9c472245548
guilherme_miller <gktmiller@gmail.com>
2024-03-22 00:24:10 -0300

    ex biblioteca da prova 3

---
+++ ex_prova/hello.c
@@ -0,0 +1,5 @@
+#include "hello.h"
+
+void hello(char nome[]){
+    printf("Ola %s\n",nome);
+}
---
+++ ex_prova/hello.h
@@ -0,0 +1,7 @@
+#ifndef VETOR_H_INCLUDED
+#define VETOR_H_INCLUDED
+#include <stdio.h>
+
+void hello(char nome[]);
+    
+#endif
