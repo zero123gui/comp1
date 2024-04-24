0eb0055fc6b302c90299cafb6bbe7cc74c541cfc
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2024-04-24 11:21:37 -0300

    ex3 completinho

--- ListasPraticas/structs_arquivos/ex3/ex3.c
+++ ListasPraticas/structs_arquivos/ex3/ex3.c
@@ -20,12 +20,17 @@ void cadastraPessoa(Pessoa pessoa[],int i){
     scanf("%d", &pessoa[i].idade);
 }
 
+void salvaArquivo(FILE *arq, int j, Pessoa pessoa[]){
+    fprintf(arq, "%s;%c;%d\n", pessoa[j].nome, pessoa[j].genero, pessoa[j].idade);
+}
+
 int main(){
     FILE *maior, *menor;
     Pessoa pessoa[5];
 
     int op=1, i=0;
 
+    //menu de cadastro
     while (op){
         cadastraPessoa(pessoa,i);
         printf("Quer continuar 1/0\n");
@@ -34,20 +39,21 @@ int main(){
         i++;
     }
     
+    //Abertura e verificaçao dos arquivos
     maior = fopen("maioridade.txt","w");
     menor = fopen("menoridade.txt","w");
-    
     if (maior == NULL || menor == NULL){
         printf("Deu erro\n");
         return 1;
     }
     
+    //salva as pessoas cadastradas no arquivo
     for (int j = 0; j < i; j++)
     {
         if (pessoa[j].idade >= 18){
-            fprintf(maior, "%s;%c;%d\n", pessoa[j].nome, pessoa[j].genero, pessoa[j].idade);
+            salvaArquivo(maior,j,pessoa);
         } else if (pessoa[j].idade < 18){
-            fprintf(menor, "%s;%c;%d\n", pessoa[j].nome, pessoa[j].genero, pessoa[j].idade);
+            salvaArquivo(menor,j,pessoa);
         }
     }
 
