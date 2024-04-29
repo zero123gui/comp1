31c9f57a5001f6c4db44bd89996452211babeb14
guilherme_miller <gktmiller@gmail.com>
2024-04-28 22:16:09 -0300

    pets 2/4

--- ListasPraticas/structs_arquivos/pets/pets.c
+++ ListasPraticas/structs_arquivos/pets/pets.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-void contaPets(FILE *arq){
+typedef struct Pets{
+    int tipo;
+    char sexo;
+    char nome[20];
+}Pets;
+
+int contaPets(FILE *arq){
     char caracter;
     int pets=0;
     while ((caracter = fgetc(arq))!=EOF)
@@ -12,10 +18,23 @@ void contaPets(FILE *arq){
         
     }
     printf("Existem %d pets\n",pets);
+    rewind(arq);
+    return pets;
+}
+
+void cadastraPets(FILE *arq, Pets pets[], int qnt){
+
+    for (int i = 0; i < qnt; i++)
+    {
+        fscanf(arq, "%d;%c;%s", &pets[i].tipo,&pets[i].sexo,pets[i].nome);
+    }
+    rewind(arq);
 }
 
 int main(){
     FILE *fr;
+    Pets pets[300];
+    int quantidade;
 
     fr = fopen("pets.txt","r");
     if (!fr)
@@ -24,8 +43,12 @@ int main(){
         return 1;
     }
 
-    contaPets(fr);
+    quantidade = contaPets(fr);
     
+    cadastraPets(fr,pets,quantidade);
+
+
+
     fclose(fr);
 
     return 0;
