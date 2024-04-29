3c107fea796761b18443fc765c5619469db062a8
guilherme_miller <gktmiller@gmail.com>
2024-04-28 22:43:51 -0300

    pets 3/4

--- ListasPraticas/structs_arquivos/pets/pets.c
+++ ListasPraticas/structs_arquivos/pets/pets.c
@@ -31,25 +31,44 @@ void cadastraPets(FILE *arq, Pets pets[], int qnt){
     rewind(arq);
 }
 
+void separaSexo(FILE *f1, FILE *f2, Pets pets[], int qnt){
+
+    for (int i = 0; i < qnt; i++)
+    {
+        if (pets[i].sexo == 'm')
+        {
+            fprintf(f1, "%d %s\n", pets[i].tipo, pets[i].nome);
+        } else {
+            fprintf(f2, "%d %s\n", pets[i].tipo, pets[i].nome);
+        }
+    }
+    rewind(f1);
+    rewind(f2);
+}
+
+
 int main(){
-    FILE *fr;
+    FILE *fr,*f1,*f2;
     Pets pets[300];
     int quantidade;
 
     fr = fopen("pets.txt","r");
-    if (!fr)
-    {
+    f1 = fopen("machos.txt","w+");
+    f2 = fopen("femeas.txt","w+");
+    if (!fr){
         printf("Erro no arquivo\n");
         return 1;
     }
 
     quantidade = contaPets(fr);
     
-    cadastraPets(fr,pets,quantidade);
+    cadastraPets(fr, pets, quantidade);
 
-
+    separaSexo(f1, f2, pets, quantidade);
 
     fclose(fr);
+    fclose(f1);
+    fclose(f2);
 
     return 0;
 }
