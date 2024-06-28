70560613d0992d444ecf32f85064417e7afeb929
Guilherme Kenzo Takano Miller <gktmiller@gmail.com>
2024-06-28 10:31:07 -0300

    n sei oq mudou mas incomoda q ta amarelo

--- ListasTeoricas/lista13-2/ex3.c
+++ ListasTeoricas/lista13-2/ex3.c
@@ -4,7 +4,7 @@ int main(){
     int v[]={11,12,13,14,15}, *p;
     p = v;
 
-    printf("%d\n", p);
+    printf("%d\n", *p);
     printf("%d\n", &v[1]);
     printf("%d\n", &p[0]);
     printf("%d\n", p[2]);
---
+++ ListasTeoricas/lista13-2/ex7.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int *p, vet[5]={1,2,3,4,5};
+    p = vet;
+
+    return 0;
+}
